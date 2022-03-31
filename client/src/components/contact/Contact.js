f5a2358a6467ee988b7961fcd2fe598ec9e23e61
codu2 <khub0929@gmail.com>
2022-03-31 16:24:18 +0900

    socket server, client event 추가 및 chatting 기능 생성 중

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,21 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
 import "./Contact.css";
 import {
   AiOutlineBell,
   AiOutlineMail,
   AiOutlineSetting,
+  AiOutlinePicture,
   AiOutlineSend,
 } from "react-icons/ai";
-import { BsEmojiSmile } from "react-icons/bs";
 
 const Contact = () => {
   const [startChat, setStartChat] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    phoneNumber: "",
+  });
+  const [socket, setSocket] = useState(null);
+  const [chat, setChat] = useState("");
+  const [notification, setNotification] = useState([]);
+  const [receiver, setReceiver] = useState(
+    userInfo.name === "housnap" ? "john" : "housnap"
+  );
+
+  useEffect(() => {
+    setSocket(io("http://localhost:5000"));
+  }, []);
+
+  useEffect(() => {
+    socket?.emit("newUser", userInfo.name);
+  }, [socket, userInfo]);
+
+  useEffect(() => {
+    socket?.on("notification", (data) => {
+      setNotification((prev) => [
+        ...prev,
+        {
+          sender: data.name,
+          chat: data.chat,
+        },
+      ]);
+    });
+  }, [socket]);
+
+  const handleInfo = (event) => {
+    event.preventDefault();
+
+    setUserInfo({
+      ...userInfo,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChatForm = (event) => {
+    event.preventDefault();
+
+    if (userInfo.name.length !== 0 && userInfo.phoneNumber.length !== 0) {
+      setStartChat(true);
+    } else {
+      window.alert("Please enter both your name and phone number.");
+    }
+  };
 
   const handleChat = (event) => {
     event.preventDefault();
 
-    setStartChat(true);
+    setChat(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setChat("");
+
+    socket?.emit("sendChat", {
+      name: userInfo.name,
+      receiver,
+      chat: chat,
+    });
   };
 
   return (
@@ -32,7 +94,30 @@ const Contact = () => {
               We will reply within 4 hours on average and notify you in advance
               that your response may be delayed after 10 p.m.
             </p>
-            <button onClick={handleChat}>Start Online Chat</button>
+            <span>
+              If you want to start chatting, please enter your name and phone
+              number.
+            </span>
+            <div className="contact__info-input">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                onChange={handleInfo}
+                autoComplete="off"
+              />
+              <label htmlFor="phoneNumber" autoComplete="off">
+                Phone Number
+              </label>
+              <input
+                type="text"
+                name="phoneNumber"
+                id="phoneNumber"
+                onChange={handleInfo}
+              />
+            </div>
+            <button onClick={handleChatForm}>Start Online Chat</button>
           </div>
         )}
         {startChat && (
@@ -43,7 +128,7 @@ const Contact = () => {
                   src={`https://images.unsplash.com/photo-1517840901100-8179e982acb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80`}
                   alt="housnap img"
                 />
-                <div className="contact__form-user-name">housnap staff</div>
+                <div className="contact__form-user-name">{userInfo.name}</div>
               </div>
               <div className="contact__form-action">
                 <div>
@@ -59,14 +144,47 @@ const Contact = () => {
                 </div>
               </div>
             </div>
-            <div className="contact__form-chat">chat</div>
+            <div className="contact__form-chat">
+              <div className="contact__form-chat-default">
+                <div className="contact__form-chat-default-info">
+                  <img
+                    src={`https://images.unsplash.com/photo-1517840901100-8179e982acb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80`}
+                    alt="housnap reception img"
+                  />
+                  <div className="contact__form-chat-default-chat">
+                    <div>
+                      Hello, <span>{userInfo.name}</span>. <br /> What kind of
+                      help do you?
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {notification.map((data) => (
+                <div className="contact__form-chat-default">
+                  <div className="contact__form-chat-default-info">
+                    <img
+                      src={`https://images.unsplash.com/photo-1517840901100-8179e982acb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80`}
+                      alt="housnap reception img"
+                    />
+                    <div className="contact__form-chat-default-chat">
+                      <div>{data.chat}</div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
             <div className="contact__form-input">
               <div className="contact__form-input-wrapper">
-                <div className="contact__form-input-emoji">
-                  <BsEmojiSmile />
+                <div className="contact__form-input-picture">
+                  <AiOutlinePicture />
                 </div>
-                <input type="text" placeholder="Start Chatting" />
-                <button>
+                <input
+                  type="text"
+                  placeholder="Start Chatting"
+                  onChange={handleChat}
+                  value={chat}
+                />
+                <button onClick={handleSubmit}>
                   <AiOutlineSend />
                 </button>
               </div>
--- socket/index.js
+++ socket/index.js
@@ -6,9 +6,35 @@ const io = new Server({
   },
 });
 
+let users = [];
+
+const addNewUser = (name, socketId) => {
+  !users.some((user) => user.name === name) && users.push({ name, socketId });
+};
+
+const findUser = (name) => {
+  return users.find((user) => user.name === name);
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
 io.on("connection", (socket) => {
+  socket.on("newUser", (name) => {
+    addNewUser(name, socket.id);
+  });
+
+  socket.on("sendChat", ({ name, receiver, chat }) => {
+    const receiveUser = findUser(receiver);
+    io.to(receiveUser.socketId).emit("notification", {
+      name,
+      chat,
+    });
+  });
+
   socket.on("disconnect", () => {
-    console.log("Someone has left");
+    removeUser(socket.id);
   });
 });
 
